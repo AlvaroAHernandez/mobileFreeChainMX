f822981c523a221bcdcf5ec208c46a78768a6ccf
RobertoLD <mexicandevil@outlook.com>
2025-10-29 17:45:26 -0700

    Creacion de la pantalla, detalles de motoclubs

---
+++ app/screens/club/clubDetail.tsx
@@ -0,0 +1,22 @@
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+import { useGlobalStyles } from '@/constants/globalStyles';
+import React from 'react';
+import { ScrollView } from 'react-native';
+
+export default function ChatScreen() {
+  const gs = useGlobalStyles();
+
+  return (
+    <ThemedView style={gs.screen}>
+      <ScrollView contentContainerStyle={{ padding: 20 }}>
+        <ThemedText style={[gs.textPrimary, { fontSize: 22, fontWeight: '600' }]}>
+          Chat
+        </ThemedText>
+        <ThemedText style={[gs.textSecondary, { marginTop: 10 }]}>
+          Aquí podrás ver los detalles de los Motoclubs.
+        </ThemedText>
+      </ScrollView>
+    </ThemedView>
+  );
+}
